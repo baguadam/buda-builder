1662f033accf9ead3a8337586b945f8150f99f81
baguadam <baguadam@gmail.com>
2023-12-26 15:58:25 +0100

    Add project files.

---
+++ MyApp.cpp
@@ -0,0 +1,392 @@
+#include "MyApp.h"
+#include "SDL_GLDebugMessageCallback.h"
+#include "ObjParser.h"
+#include "ParametricSurfaceMesh.hpp"
+
+#include <imgui.h>
+
+CMyApp::CMyApp()
+{
+}
+
+CMyApp::~CMyApp()
+{
+}
+
+void CMyApp::SetupDebugCallback()
+{
+	// engedélyezzük és állítsuk be a debug callback függvényt ha debug context-ben vagyunk 
+	GLint context_flags;
+	glGetIntegerv(GL_CONTEXT_FLAGS, &context_flags);
+	if (context_flags & GL_CONTEXT_FLAG_DEBUG_BIT) {
+		glEnable(GL_DEBUG_OUTPUT);
+		glEnable(GL_DEBUG_OUTPUT_SYNCHRONOUS);
+		glDebugMessageControl(GL_DONT_CARE, GL_DONT_CARE, GL_DEBUG_SEVERITY_NOTIFICATION, 0, nullptr, GL_FALSE);
+		glDebugMessageCallback(SDL_GLDebugMessageCallback, nullptr);
+	}
+}
+
+void CMyApp::InitShaders()
+{
+	m_programID = glCreateProgram();
+	AssembleProgram( m_programID, "Vert_PosNormTex.vert", "Frag_Lighting.frag" );
+
+}
+
+void CMyApp::CleanShaders()
+{
+	glDeleteProgram( m_programID );
+}
+
+// Nyers parameterek
+struct Param
+{
+	glm::vec3 GetPos( float u, float v ) const noexcept
+	{
+		return glm::vec3( u, v, 0.0f );
+	}
+	glm::vec3 GetNorm( float u, float v ) const noexcept
+	{
+		return glm::vec3( 0.0,0.0,1.0 );
+	}
+	glm::vec2 GetTex( float u, float v ) const noexcept
+	{
+		return glm::vec2( u, v );
+	}
+};
+
+struct Sphere
+{
+	float r;
+	Sphere(float _r = 1.f) : r(_r) { }
+
+	glm::vec3 GetPos(float u, float v) const noexcept
+	{
+		u *= glm::two_pi<float>();
+		v *= glm::pi<float>();
+
+		return glm::vec3(
+			r * sinf(v) * cosf(u),
+			r * cosf(v),
+			r * sinf(v) * sinf(u)
+		);
+	}
+	glm::vec3 GetNorm(float u, float v) const noexcept
+	{
+		u *= glm::two_pi<float>();
+		v *= glm::pi<float>();
+
+		return glm::vec3(
+			sinf(v) * cosf(u),
+			cosf(v),
+			sinf(v) * sinf(u)
+		);
+	}
+	glm::vec2 GetTex(float u, float v) const noexcept
+	{
+		return glm::vec2(u, v);
+	}
+};
+
+struct Torus
+{
+	float a, b;
+	Torus(float _a = 1.0f, float _b = 2.0f) : a(_a), b(_b) { }
+
+	glm::vec3 GetPos(float u, float v) const noexcept
+	{
+		u *=  glm::two_pi<float>();
+		v *= -glm::two_pi<float>();
+		return glm::vec3(
+			(a * cosf(v) + b) * cosf(u),
+			 a * sinf(v),
+			(a * cosf(v) + b) * sinf(u)
+		);
+	}
+	glm::vec3 GetNorm(float u, float v) const noexcept
+	{
+		glm::vec3 du = GetPos(u + 0.01f, v) - GetPos(u - 0.01f, v);
+		glm::vec3 dv = GetPos(u, v + 0.01f) - GetPos(u, v - 0.01f);
+
+		return glm::normalize(glm::cross(du, dv));
+	}
+	glm::vec2 GetTex(float u, float v) const noexcept
+	{
+		return glm::vec2(u, v);
+	}
+};
+
+struct SurfaceOfRevolution
+{
+	float ProfileFunc(float t) const noexcept
+	{
+		// return 1.0f; // Henger
+		return 1 - t;
+	}
+
+	glm::vec3 GetPos(float u, float v) const noexcept
+	{
+		u *= glm::two_pi<float>();
+
+		float r = ProfileFunc(v);
+
+		return glm::vec3(
+			r * cosf(u),
+			v,
+			- r * sinf(u)
+		);
+	}
+	glm::vec3 GetNorm(float u, float v) const noexcept
+	{
+		return glm::vec3(0.0, 0.0, 1.0);
+	}
+	glm::vec2 GetTex(float u, float v) const noexcept
+	{
+		return glm::vec2(u, v);
+	}
+};
+
+void CMyApp::InitGeometry()
+{
+
+	const std::initializer_list<VertexAttributeDescriptor> vertexAttribList =
+	{
+		{ 0, offsetof( Vertex, position ), 3, GL_FLOAT },
+		{ 1, offsetof( Vertex, normal   ), 3, GL_FLOAT },
+		{ 2, offsetof( Vertex, texcoord ), 2, GL_FLOAT },
+	};
+
+	MeshObject<Vertex> surfaceMeshCPU = GetParamSurfMesh( Sphere() );
+	m_surfaceGPU = CreateGLObjectFromMesh( surfaceMeshCPU, vertexAttribList );
+}
+
+void CMyApp::CleanGeometry()
+{
+	CleanOGLObject( m_surfaceGPU );
+}
+
+void CMyApp::InitTextures()
+{
+	// diffuse texture
+	glGenTextures( 1, &m_TextureID );
+	TextureFromFile( m_TextureID, "Assets/filipp.jpg" );
+	SetupTextureSampling( GL_TEXTURE_2D, m_TextureID );
+}
+
+void CMyApp::CleanTextures()
+{
+	glDeleteTextures( 1, &m_TextureID );
+}
+
+bool CMyApp::Init()
+{
+	SetupDebugCallback();
+
+	// törlési szín legyen kékes
+	glClearColor(0.125f, 0.25f, 0.5f, 1.0f);
+
+	InitShaders();
+	InitGeometry();
+	InitTextures();
+
+	//
+	// egyéb inicializálás
+	//
+
+	glEnable(GL_CULL_FACE); // kapcsoljuk be a hátrafelé néző lapok eldobását
+	glCullFace(GL_BACK);    // GL_BACK: a kamerától "elfelé" néző lapok, GL_FRONT: a kamera felé néző lapok
+
+	glEnable(GL_DEPTH_TEST); // mélységi teszt bekapcsolása (takarás)
+
+	// kamera
+	m_camera.SetView(
+		glm::vec3(0.0, 7.0, 7.0),// honnan nézzük a színteret	   - eye
+		glm::vec3(0.0, 0.0, 0.0),   // a színtér melyik pontját nézzük - at
+		glm::vec3(0.0, 1.0, 0.0));  // felfelé mutató irány a világban - up
+
+	return true;
+}
+
+void CMyApp::Clean()
+{
+	CleanShaders();
+	CleanGeometry();
+	CleanTextures();
+}
+
+void CMyApp::Update( const SUpdateInfo& updateInfo )
+{
+	m_ElapsedTimeInSec = updateInfo.ElapsedTimeInSec;
+
+	m_camera.Update( updateInfo.DeltaTimeInSec );
+	m_lightPos = glm::vec4( m_camera.GetEye(), 1.0 );
+}
+
+void CMyApp::Render()
+{
+	// töröljük a frampuffert (GL_COLOR_BUFFER_BIT)...
+	// ... és a mélységi Z puffert (GL_DEPTH_BUFFER_BIT)
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	// - VAO beállítása
+	glBindVertexArray( m_surfaceGPU.vaoID );
+
+	// - Textúrák beállítása, minden egységre külön
+	glActiveTexture( GL_TEXTURE0 );
+	glBindTexture( GL_TEXTURE_2D, m_TextureID );
+
+	glUseProgram( m_programID );
+
+	glm::mat4 matWorld = glm::identity<glm::mat4>();
+
+	glUniformMatrix4fv( ul( "world" ),    1, GL_FALSE, glm::value_ptr( matWorld ) );
+	glUniformMatrix4fv( ul( "worldIT" ),  1, GL_FALSE, glm::value_ptr( glm::transpose( glm::inverse( matWorld ) ) ) );
+	glUniformMatrix4fv( ul( "viewProj" ), 1, GL_FALSE, glm::value_ptr( m_camera.GetViewProj() ) );
+
+	// - Fényforrások beállítása
+	glUniform3fv( ul( "cameraPos" ), 1, glm::value_ptr( m_camera.GetEye() ) );
+	glUniform4fv( ul( "lightPos" ),  1, glm::value_ptr( m_lightPos ) );
+
+	glUniform3fv( ul( "La" ),		 1, glm::value_ptr( m_La ) );
+	glUniform3fv( ul( "Ld" ),		 1, glm::value_ptr( m_Ld ) );
+	glUniform3fv( ul( "Ls" ),		 1, glm::value_ptr( m_Ls ) );
+
+	glUniform1f( ul( "lightConstantAttenuation"	 ), m_lightConstantAttenuation );
+	glUniform1f( ul( "lightLinearAttenuation"	 ), m_lightLinearAttenuation   );
+	glUniform1f( ul( "lightQuadraticAttenuation" ), m_lightQuadraticAttenuation);
+
+	// - Anyagjellemzők beállítása
+	glUniform3fv( ul( "Ka" ),		 1, glm::value_ptr( m_Ka ) );
+	glUniform3fv( ul( "Kd" ),		 1, glm::value_ptr( m_Kd ) );
+	glUniform3fv( ul( "Ks" ),		 1, glm::value_ptr( m_Ks ) );
+
+	glUniform1f( ul( "Shininess" ),	m_Shininess );
+
+	// - textúraegységek beállítása
+	glUniform1i( ul( "texImage" ), 0 );
+
+	glDrawElements( GL_TRIANGLES,    
+					m_surfaceGPU.count,			 
+					GL_UNSIGNED_INT,
+					nullptr );
+
+	// shader kikapcsolasa
+	glUseProgram( 0 );
+
+	// - Textúrák kikapcsolása, minden egységre külön
+	glActiveTexture( GL_TEXTURE0 );
+	glBindTexture( GL_TEXTURE_2D, 0 );
+
+	// VAO kikapcsolása
+	glBindVertexArray( 0 );
+}
+
+void CMyApp::RenderGUI()
+{
+	// ImGui::ShowDemoWindow();
+	if ( ImGui::Begin( "Lighting settings" ) )
+	{
+		ImGui::SeparatorText("Settings");
+		
+		//ImGui::SliderFloat("Shininess", &m_Shininess, 0.0001f, 10.0f );
+		ImGui::InputFloat("Shininess", &m_Shininess, 0.1f, 1.0f, "%.1f" );
+		static float Kaf = 1.0f;
+		static float Kdf = 1.0f;
+		static float Ksf = 1.0f;
+		if ( ImGui::SliderFloat( "Ka", &Kaf, 0.0f, 1.0f ) )
+		{
+			m_Ka = glm::vec3( Kaf );
+		}
+		if ( ImGui::SliderFloat( "Kd", &Kdf, 0.0f, 1.0f ) )
+		{
+			m_Kd = glm::vec3( Kdf );
+		}
+		if ( ImGui::SliderFloat( "Ks", &Ksf, 0.0f, 1.0f ) )
+		{
+			m_Ks = glm::vec3( Ksf );
+		}
+
+		ImGui::SliderFloat( "Constant Attenuation", &m_lightConstantAttenuation, 0.001f, 2.0f );
+		ImGui::SliderFloat( "Linear Attenuation", &m_lightLinearAttenuation, 0.001f, 2.0f );
+		ImGui::SliderFloat( "Quadratic Attenuation", &m_lightQuadraticAttenuation, 0.001f, 2.0f );	
+	}
+
+	ImGui::End();
+}
+
+GLint CMyApp::ul( const char* uniformName ) noexcept
+{
+	GLuint programID = 0;
+
+	// Kérdezzük le az aktuális programot!
+	// https://registry.khronos.org/OpenGL-Refpages/gl4/html/glGet.xhtml
+	glGetIntegerv( GL_CURRENT_PROGRAM, reinterpret_cast<GLint*>( &programID ) );
+	// A program és a uniform név ismeretében kérdezzük le a location-t!
+	// https://registry.khronos.org/OpenGL-Refpages/gl4/html/glGetUniformLocation.xhtml
+	return glGetUniformLocation( programID, uniformName );
+}
+
+// https://wiki.libsdl.org/SDL2/SDL_KeyboardEvent
+// https://wiki.libsdl.org/SDL2/SDL_Keysym
+// https://wiki.libsdl.org/SDL2/SDL_Keycode
+// https://wiki.libsdl.org/SDL2/SDL_Keymod
+
+void CMyApp::KeyboardDown(const SDL_KeyboardEvent& key)
+{	
+	if ( key.repeat == 0 ) // Először lett megnyomva
+	{
+		if ( key.keysym.sym == SDLK_F5 && key.keysym.mod & KMOD_CTRL )
+		{
+			CleanShaders();
+			InitShaders();
+		}
+		if ( key.keysym.sym == SDLK_F1 )
+		{
+			GLint polygonModeFrontAndBack[ 2 ] = {};
+			// https://registry.khronos.org/OpenGL-Refpages/gl4/html/glGet.xhtml
+			glGetIntegerv( GL_POLYGON_MODE, polygonModeFrontAndBack ); // Kérdezzük le a jelenlegi polygon módot! Külön adja a front és back módokat.
+			GLenum polygonMode = ( polygonModeFrontAndBack[ 0 ] != GL_FILL ? GL_FILL : GL_LINE ); // Váltogassuk FILL és LINE között!
+			// https://registry.khronos.org/OpenGL-Refpages/gl4/html/glPolygonMode.xhtml
+			glPolygonMode( GL_FRONT_AND_BACK, polygonMode ); // Állítsuk be az újat!
+		}
+	}
+	m_camera.KeyboardDown( key );
+}
+
+void CMyApp::KeyboardUp(const SDL_KeyboardEvent& key)
+{
+	m_camera.KeyboardUp( key );
+}
+
+// https://wiki.libsdl.org/SDL2/SDL_MouseMotionEvent
+
+void CMyApp::MouseMove(const SDL_MouseMotionEvent& mouse)
+{
+	m_camera.MouseMove( mouse );
+}
+
+// https://wiki.libsdl.org/SDL2/SDL_MouseButtonEvent
+
+void CMyApp::MouseDown(const SDL_MouseButtonEvent& mouse)
+{
+}
+
+void CMyApp::MouseUp(const SDL_MouseButtonEvent& mouse)
+{
+}
+
+// https://wiki.libsdl.org/SDL2/SDL_MouseWheelEvent
+
+void CMyApp::MouseWheel(const SDL_MouseWheelEvent& wheel)
+{
+	m_camera.MouseWheel( wheel );
+}
+
+
+// a két paraméterben az új ablakméret szélessége (_w) és magassága (_h) található
+void CMyApp::Resize(int _w, int _h)
+{
+	glViewport(0, 0, _w, _h);
+	m_camera.Resize( _w, _h );
+}
+
---
+++ MyApp.h
@@ -0,0 +1,105 @@
+#pragma once
+
+// GLM
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <glm/gtx/transform2.hpp>
+
+// GLEW
+#include <GL/glew.h>
+
+// SDL
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_opengl.h>
+
+// Utils
+#include "GLUtils.hpp"
+#include "Camera.h"
+
+struct SUpdateInfo
+{
+	float ElapsedTimeInSec = 0.0f; // Program indulása óta eltelt idő
+	float DeltaTimeInSec   = 0.0f; // Előző Update óta eltelt idő
+};
+
+class CMyApp
+{
+public:
+	CMyApp();
+	~CMyApp();
+
+	bool Init();
+	void Clean();
+
+	void Update( const SUpdateInfo& );
+	void Render();
+	void RenderGUI();
+
+	void KeyboardDown(const SDL_KeyboardEvent&);
+	void KeyboardUp(const SDL_KeyboardEvent&);
+	void MouseMove(const SDL_MouseMotionEvent&);
+	void MouseDown(const SDL_MouseButtonEvent&);
+	void MouseUp(const SDL_MouseButtonEvent&);
+	void MouseWheel(const SDL_MouseWheelEvent&);
+	void Resize(int, int);
+protected:
+	void SetupDebugCallback();
+
+	//
+	// Adat változók
+	//
+
+	float m_ElapsedTimeInSec = 0.0f;
+
+	// Kamera
+	Camera m_camera;
+
+	//
+	// OpenGL-es dolgok
+	//
+	
+	// uniform location lekérdezése
+	GLint ul( const char* uniformName ) noexcept;
+
+	// shaderekhez szükséges változók
+	GLuint m_programID = 0; // shaderek programja
+
+	// Fényforrás- ...
+	glm::vec4 m_lightPos = glm::vec4( 0.0f, 0.0f, 1.0f, 0.0f );
+
+	glm::vec3 m_La = glm::vec3(0.0, 0.0, 0.0 );
+	glm::vec3 m_Ld = glm::vec3(1.0, 1.0, 1.0 );
+	glm::vec3 m_Ls = glm::vec3(1.0, 1.0, 1.0 );
+
+	float m_lightConstantAttenuation    = 1.0;
+	float m_lightLinearAttenuation      = 0.0;
+	float m_lightQuadraticAttenuation   = 0.0;
+
+	// ... és anyagjellemzők
+	glm::vec3 m_Ka = glm::vec3( 1.0 );
+	glm::vec3 m_Kd = glm::vec3( 1.0 );
+	glm::vec3 m_Ks = glm::vec3( 1.0 );
+
+	float m_Shininess = 1.0;
+
+	// Shaderek inicializálása, és törtlése
+	void InitShaders();
+	void CleanShaders();
+
+	// Geometriával kapcsolatos változók
+
+	OGLObject m_surfaceGPU = {};
+
+	// Geometria inicializálása, és törtlése
+	void InitGeometry();
+	void CleanGeometry();
+
+	// Textúrázás, és változói
+
+	GLuint m_TextureID = 0;
+
+	void InitTextures();
+	void CleanTextures();
+};
+
